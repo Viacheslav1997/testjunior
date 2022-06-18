c0ebab5abfec7ec5e77488d30063e3cc3fb849e5
Viacheslav <shapkins1997@mail.ru>
2022-06-18 14:39:37 +0500

    Добавлен роутинг

--- main.go
+++ main.go
@@ -1,1 +1,20 @@
-package testjunior
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"testjunior/handlers"
+)
+
+func main() {
+
+	http.HandleFunc("/receive", handlers.RecieveHandler)
+
+	http.HandleFunc("/refresh", handlers.RefreshHandler)
+
+	fmt.Println("Server is listening...")
+	err := http.ListenAndServe("localhost:8181", nil)
+	if err != nil {
+		return
+	}
+}
