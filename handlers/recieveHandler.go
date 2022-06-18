45ca2879bba629c0593b8633ba220c74ff54e486
Viacheslav <shapkins1997@mail.ru>
2022-06-18 14:37:17 +0500

    Добавлены хэндлеры для двух REST маршрутов

---
+++ handlers/recieveHandler.go
@@ -0,0 +1,49 @@
+package handlers
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"testjunior/database"
+	"testjunior/tokens"
+)
+
+type Tokens struct {
+	Access_token  string
+	Refresh_token string
+}
+
+func RecieveHandler(w http.ResponseWriter, r *http.Request) {
+
+	guid := r.URL.Query().Get("GUID")
+
+	res, err, _ := database.Check_session(guid)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if res == 1 {
+		fmt.Fprint(w, "Session for this user already exist")
+	} else if res == 0 {
+		access_token, refresh_token64 := tokens.GenerateAllTokens(guid)
+		refresh_hash := tokens.Bcrypt(refresh_token64)
+		database.Save_tokens(refresh_hash, guid)
+
+		t := Tokens{
+			Access_token:  access_token,
+			Refresh_token: refresh_token64,
+		}
+
+		mess, err := json.Marshal(t)
+
+		if err != nil {
+			log.Panic(err)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(mess)
+	}
+
+}
---
+++ handlers/refreshHandler.go
@@ -0,0 +1,66 @@
+package handlers
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"testjunior/database"
+	"testjunior/models"
+	"testjunior/tokens"
+)
+
+type Foo struct {
+	Token string
+}
+
+func RefreshHandler(w http.ResponseWriter, r *http.Request) {
+
+	//получаем id user-а и base64 refresh token
+	guid := r.URL.Query().Get("GUID")
+	var user_refresh_token Foo
+	err := json.NewDecoder(r.Body).Decode(&user_refresh_token)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//проверяем, есть ли юзер с таким id в бд
+	session_check_res, err, result := database.Check_session(guid)
+	if err != nil {
+		result = models.User{Refresh_token: nil}
+		log.Fatal(err)
+	}
+
+	//проверяем, валидный ли его refresh base64 токен
+	//
+	token_check_res, _ := database.Check_refresh_token(user_refresh_token.Token, result)
+	if (session_check_res == 1) && (token_check_res) {
+
+		//Удаляем старый
+		database.DeleteClientSessionByGUID(guid)
+		//Создаем два новых токена
+		access_token, refresh_token64 := tokens.GenerateAllTokens(guid)
+		refresh_hash := tokens.Bcrypt(refresh_token64)
+		database.Save_tokens(refresh_hash, guid)
+
+		t := Tokens{
+			Access_token:  access_token,
+			Refresh_token: refresh_token64,
+		}
+
+		mess, err := json.Marshal(t)
+
+		if err != nil {
+			log.Panic(err)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(mess)
+		//fmt.Fprint(w, "Можно делать рефреш\n")
+		//fmt.Fprint(w, result)
+	} else if (session_check_res == 1) && (!token_check_res) {
+		fmt.Fprint(w, "Invalid refresh session (your refresh session incorrect)\n")
+	} else if session_check_res == 0 {
+		fmt.Fprint(w, "For this user no accessible session (no user with this GUID)\n")
+	}
+}
