97f1338445c1d03e386c9295b894efde7931f765
Viacheslav <shapkins1997@mail.ru>
2022-06-19 12:05:44 +0500

    Измененеия проверки токенов

--- handlers/recieveHandler.go
+++ handlers/recieveHandler.go
@@ -2,7 +2,6 @@ package handlers
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
 	"testjunior/database"
@@ -19,33 +18,24 @@ func RecieveHandler(w http.ResponseWriter, r *http.Request) {
 	case http.MethodPost:
 		guid := r.URL.Query().Get("GUID")
 
-		res, err, _ := database.Check_session(guid)
+		access_token, refresh_token64 := tokens.GenerateAllTokens(guid)
+		refresh_hash := tokens.Bcrypt(refresh_token64)
+		database.Save_tokens(refresh_hash, guid)
 
+		t := Tokens{
+			Access_token:  access_token,
+			Refresh_token: refresh_token64,
+		}
+
+		mess, err := json.Marshal(t)
 		if err != nil {
-			log.Fatal(err)
+			log.Panic(err)
+			return
 		}
 
-		if res {
-			fmt.Fprint(w, "Session for this user already exist")
-		} else if !res {
-			access_token, refresh_token64 := tokens.GenerateAllTokens(guid)
-			refresh_hash := tokens.Bcrypt(refresh_token64)
-			database.Save_tokens(refresh_hash, guid)
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(mess)
 
-			t := Tokens{
-				Access_token:  access_token,
-				Refresh_token: refresh_token64,
-			}
-
-			mess, err := json.Marshal(t)
-			if err != nil {
-				log.Panic(err)
-				return
-			}
-
-			w.Header().Set("Content-Type", "application/json")
-			w.Write(mess)
-		}
 	default:
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 	}
--- handlers/refreshHandler.go
+++ handlers/refreshHandler.go
@@ -2,43 +2,38 @@ package handlers
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
 	"testjunior/database"
-	"testjunior/models"
 	"testjunior/tokens"
 )
 
 type Foo struct {
-	Token string
+	RefreshToken string
+	AccessToken  string
 }
 
 func RefreshHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodPost:
-		//получаем id user-а и base64 refresh token
+		//получаем id user-а и tokens
 		guid := r.URL.Query().Get("GUID")
-		var user_refresh_token Foo
-		err := json.NewDecoder(r.Body).Decode(&user_refresh_token)
+		var user_tokens Foo
+		err := json.NewDecoder(r.Body).Decode(&user_tokens)
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		//проверяем, есть ли юзер с таким id в бд
-		session_check_res, err, result := database.Check_session(guid)
-		if err != nil {
-			result = models.User{Refresh_token: nil}
-			log.Fatal(err)
-		}
+		//проверяем, есть ли юзер с таким id и рефреш токеном в бд
+		session_check_res, err, res := database.Check_session(guid, user_tokens.RefreshToken)
 
-		//проверяем, валидный ли его refresh base64 токен
-		//
-		token_check_res, _ := database.Check_refresh_token(user_refresh_token.Token, result)
-		if (session_check_res) && (token_check_res) {
+		//проверяем, был ли выдан данный access токен вместе с этим refresh
+		tokens_coonect_res := database.ValidateSession(user_tokens.RefreshToken, user_tokens.AccessToken)
+
+		if (session_check_res) && (tokens_coonect_res) {
 
 			//Удаляем старый
-			database.DeleteClientSessionByGUID(guid)
+			database.DeleteClientSessionByGUID(guid, res.Refresh_token)
 			//Создаем два новых токена
 			access_token, refresh_token64 := tokens.GenerateAllTokens(guid)
 			refresh_hash := tokens.Bcrypt(refresh_token64)
@@ -57,12 +52,9 @@ func RefreshHandler(w http.ResponseWriter, r *http.Request) {
 			}
 			w.Header().Set("Content-Type", "application/json")
 			w.Write(mess)
-			//fmt.Fprint(w, "Можно делать рефреш\n")
-			//fmt.Fprint(w, result)
-		} else if (session_check_res) && (!token_check_res) {
-			fmt.Fprint(w, "Invalid refresh session (your refresh session incorrect)\n")
-		} else if !session_check_res {
-			fmt.Fprint(w, "For this user no accessible session (no user with this GUID)\n")
+
+		} else {
+			http.Error(w, "Error", http.StatusBadRequest)
 		}
 	default:
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
